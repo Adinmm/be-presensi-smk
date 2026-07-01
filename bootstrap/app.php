6955da629488d6731a49caadfba832038c88d9d2
Adinmm <muhammadmuayyadin@gmail.com>
2026-07-01 14:51:44 +0800

    up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,26 +9,26 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
+        // api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-        then: function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/student.php'));
+        // then: function () {
+        //     Route::middleware('api') 
+        //         ->prefix('api')
+        //         ->group(base_path('routes/student.php'));
 
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/kelas.php'));
+        //     Route::middleware('api')
+        //         ->prefix('api')
+        //         ->group(base_path('routes/kelas.php'));
 
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/attendence.php'));
+        //     Route::middleware('api')
+        //         ->prefix('api')
+        //         ->group(base_path('routes/attendence.php'));
 
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/report.php'));
-        }
+        //     Route::middleware('api')
+        //         ->prefix('api')
+        //         ->group(base_path('routes/report.php'));
+        // }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
