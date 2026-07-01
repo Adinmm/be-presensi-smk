acb52266bbcde043117e12ac2057054835c4bb8b
Adinmm <muhammadmuayyadin@gmail.com>
2026-07-01 15:53:18 +0800

    up

--- api/index.php
+++ api/index.php
@@ -1,8 +1,3 @@
 <?php
 
 require __DIR__.'/../public/index.php';
-
-require __DIR__.'/student.php';
-require __DIR__.'/report.php';
-require __DIR__.'/kelas.php';
-require __DIR__.'/attendence.php';
--- routes/student.php
+++ routes/student.php
@@ -4,11 +4,6 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::post('/student', [StudentController::class, 'store']);
 Route::get('/students', [StudentController::class, 'index']);
 Route::patch('/student/{id}', [StudentController::class, 'update']);
